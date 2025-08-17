f419f748cc56b8f3542f9f3b0f6f8e2071323a13
AkshayShetty <akshayshetty730@gmail.com>
2025-08-17 18:28:27 +0530

    TargetSdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.akshay.statuscatch"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 3
         versionName = "1.1"
 
